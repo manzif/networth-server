eea84fb8f7026e35d3be9bd7c59c490c6c8c376b
Manzi Fabrice <manzif60@gmail.com>
2020-08-24 18:18:51 +0200

    add profile

--- src/controllers/profiles.js
+++ src/controllers/profiles.js
@@ -24,29 +24,26 @@ class ProfileManager {
       if (req.file) {
         const file = dataUri(req).content;
         const result = await uploader.upload(file);
-        req.body.image = result.url;
+        req.body.profile = result.url;
       }
-
-      const data = await User.findOne({ where: { username: req.params.username } });
-      if(data){
-        const updated = await data.update({
-          firstname: req.body.firstname || data.firstname,
-          lastname: req.body.lastname || data.lastname,
-          email: req.body.email || data.email,
-          username: req.body.username || data.username,
-          profile: req.body.profile || data.profile,
-          role: req.body.role || data.role
-        });
-        updated.password = undefined;
-        return res.status(200).json({
-          user: updated
-        });
-      }
-      return res.status(400).json({
-        message: 'user not found'
-      });
+      console.log('\n\n\n\n\n\n', req.body.profile)
+      // const data = await User.findOne({ where: { username: req.params.username } });
+      // console.log('\n\n\n\n\n\n', req.body.profile)
+      // const updated = await data.update({
+      //   firstname: req.body.firstname || data.dataValues.firstname,
+      //   lastname: req.body.lastname || data.lastname,
+      //   email: req.body.email || data.email,
+      //   username: req.body.username || data.username,
+      //   profile: req.body.profile || data.dataValues.profile
+      // });
+      // updated.password = undefined;
+      // return res.status(200).json({
+      //   user: updated
+      // });
     } catch (error) {
-      return res.json({ error });
+      return res.status(400).json({
+        message: error.message
+      });
     }
   }
 
@@ -62,6 +59,43 @@ class ProfileManager {
       return res.status(500).json({ error });
     }
   }
+
+  // static async addProduct(req, res) {
+  //   try {
+  //     const data = await User.findOne({ where: { username: req.params.username } });
+  //     if(data){
+  //       if(req.file) {
+  //         const dataBuffer = new Buffer.from(req.file.Buffer);
+  //         console.log('\n\n\n\n\n\n', 'test')
+  //         const mediaType = path.extname(req.file.originalname).toString();
+  //         const imageData = imageDataURI.encode(dataBuffer, mediaType);
+  //         const uploadedImage = await uploader.upload(imageData);
+  //         const image= uploadedImage.url
+  //       }
+        
+  
+  //       const updated = await data.update({
+  //         firstname: req.body.firstname || data.firstname,
+  //         lastname: req.body.lastname || data.lastname,
+  //         email: req.body.email || data.email,
+  //         username: req.body.username || data.username,
+  //         profile: image || data.profile
+  //       });
+  //       return res.status(201).send({
+  //         success: true,
+  //         message: 'Product added'
+  //       });
+  //     }
+  //     return res.status(400).json({
+  //       message: 'user not found'
+  //     });
+  //   } catch (error) {
+  //     return res.status(500).send({
+  //       message: error.message
+  //     });
+  //   }
+  // }
+
 }
 
 export default ProfileManager;
--- src/middleware/multer.js
+++ src/middleware/multer.js
@@ -2,16 +2,12 @@
 // import Datauri from 'datauri';
 // import path from 'path';
 
-// const storage = multer.memoryStorage();
-// const multerUploads = multer({ storage }).single('image');
 // const dUri = new Datauri();
 
-// /**
-// * @description This function converts the buffer to data url
-// * @param {Object} req containing the field object
-// * @returns {String} The data url from the string buffer
-// */
+// const storage = multer.memoryStorage();
+
+// const multerUploads = multer({ storage }).single('profile');
 
 // const dataUri = req => dUri.format(path.extname(req.file.originalname).toString(), req.file.buffer);
 
-// export { multerUploads, dataUri };
+// export { multerUploads };
